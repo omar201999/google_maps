156dc47fbfb6f3e4663ef00eb6e858eb1d84a277
omar essam <essamomar528@gmail.com>
2022-09-24 11:19:23 +0200

    first commit

---
+++ lib/constnats/strings.dart
@@ -0,0 +1,10 @@
+const loginScreen = '/login-screen';
+const otpScreen = '/otp-screen';
+const mapScreen = '/map-screen';
+const googleAPIKey = 'AIzaSyB4UZQ8T5YilsYwp1su7cmaswgnr6MsZeY';
+const suggestionsBaseUrl =
+    'https://maps.googleapis.com/maps/api/place/autocomplete/json';
+const placeLocationBaseUrl =
+    'https://maps.googleapis.com/maps/api/place/details/json';
+const directionsBaseUrl =
+    'https://maps.googleapis.com/maps/api/directions/json';
